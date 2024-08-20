e67822310e0a673b4f42d124fb363e2b74017bc0
vinayki <kharevinayki7@gmail.com>
2024-08-20 16:32:00 +0530

    initial commit

---
+++ about.php
@@ -0,0 +1,32 @@
+<?php include_once "./include/header.php"; ?>
+
+<div class="container">
+    <div class="row">
+        <img src="images/logo.png" class="col-4 card-img-top" alt="Logo" width="600px" height="300px">
+
+        <div class="container col-8" style="margin-top: 50px;">
+            <div class="card">
+                <div class="card-header text-center">
+                    <strong>Vindhya Institute of Technology and Science</strong>
+                </div>
+
+                <div class="card-body">
+
+                <strong class="card-title">PROJECT MEMBERS</strong>
+                <p>
+                Here unemployed skilled person can resgistered themselves according to their profession and skills.
+                 & end users can book the service according to their daily needs. We help unemployed peoples to find
+                  the work and also the customers to get all the daily needed services on the single platform itself.
+                </p>
+            </div>
+
+            <div class="card-footer text-center">
+            Under Guidance of Miss. Kashifa Khan
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php include_once "./include/footer.php"; ?>
